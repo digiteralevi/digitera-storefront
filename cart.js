91c1f6ac9872c5279414d3fe3235c9b3d3b58bd3
digiteralevi <digiteralevi@gmail.com>
2026-07-17 13:04:18 +0800

    Update cart.js

--- cart.js
+++ cart.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID", 
   appId: "YOUR_APP_ID"
 };
 
